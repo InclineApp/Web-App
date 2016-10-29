2f6be93b9f6cbfb343e0333dcf3855518d52eddc
Jake Schroeder <jakeschroeder8@gmail.com>
2016-10-29 17:37:09 -0500

    added a test enviroment to project structure

---
+++ Test Env/assets/js/global.js
@@ -0,0 +1,8 @@
+var mastHeadHeight = $( '.masthead' ).height();
+var navbarHeight = $('#navbar').height();
+
+$(document).ready(function() {
+  $('.masthead').click(function() {
+    alert('hello, world');
+   });
+});
