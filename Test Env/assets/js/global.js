99c399de8e7dbe2dd6c46a85918d82ecb4129306
jake-dev <jakeschroeder8@gmail.com>
2016-11-03 07:50:59 -0500

    fixed layout and added video

--- Test Env/assets/js/global.js
+++ Test Env/assets/js/global.js
@@ -1,8 +1,41 @@
-var mastHeadHeight = $( '.masthead' ).height();
-var navbarHeight = $('#navbar').height();
+	
+$(window).resize(function() {
 
-$(document).ready(function() {
-  $('.masthead').click(function() {
-    alert('hello, world');
-   });
-});
+	var height = $(this).height() - $('#nav').height();
+	var width = $(this).width();
+	var smallHeight = 50;
+
+	if(width > 768) {
+		$('.masthead').height(height);
+    	$('.video-container').height(height);
+
+    	$('#middle').addClass("full-section");
+		$('#centerd-content').addClass("centerd-content");
+
+    	$("section").attr({
+        	height: $(this).height(),
+        	width: width
+        });
+
+        $("#video").attr({
+        	height: height,
+        	width: width
+    	});
+
+		$('#nav').affix({
+		      offset: {
+		        top: function() {
+                    return $('.masthead').height();
+                }
+		      }
+		});
+    
+	}
+	else {
+		$('#middle').removeClass("full-section");
+		$('#centerd-content').removeClass("centerd-content");
+	}
+
+})
+
+$(window).resize(); //on page load
